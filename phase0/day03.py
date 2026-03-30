b2491d99a466c304d87ffb3c8204b7fbb5b72618
Yash Sawant <yashsawant808@gmail.com>
2026-03-30 18:09:36 +0530

    Add day03.py

---
+++ phase0/day03.py
@@ -0,0 +1,22 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+age = int(age)  # Convert age to an integer
+print(f"Hello, {name}! You are {age} years old.")
+
+#simple calculator
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if type(num1) == int and type(num2) == int:
+    print(f"The sum of {num1} and {num2} is {num1 + num2}")
+    print(f"The product of {num1} and {num2} is {num1 * num2}")
+    print(f"The division of {num1} and {num2} is {num1 / num2}")
+else:
+    print("Please enter valid integers for the calculation.")
+
+
+
+
+
